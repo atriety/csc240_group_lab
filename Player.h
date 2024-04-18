ecffc9c04ce439e736c3110d3c5667741665f83e
anhtriet <anhtriettran@gmail.com>
2024-04-18 18:20:14 -0400

    adding shell files

---
+++ Player.h
@@ -0,0 +1,24 @@
+#ifndef PLAYER
+#define PLAYER
+
+
+class Player {
+
+};
+
+
+class Crewmate: public Player {
+
+};
+
+
+class Imposter: public Player {
+
+
+// sort function to find highest level player
+// sort function to find lowest level player
+
+};
+
+
+#endif PLAYER
